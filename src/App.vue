b1aebb6770436573de42afeeebcc3ee46a78ccf9
wyoming <andrey.dar1214@gmail.com>
2024-07-22 19:28:19 +0300

    added pagination && posts sorting

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class = "app">
+    <myInput 
+    v-model="searchQuiery"
+    />
     <div class="app__buttons">
         <myButton @click="showDialog">создать пост</myButton>
         <mySelect 
@@ -15,11 +18,18 @@
     </myDialog>
 
     <postList 
-    :posts="posts"
+    :posts="searchedSortedPosts"
     @remove="removePost"
     v-if="!isPostLoading"
     />
     <div v-else>Идет загрузка...</div>
+    <div class="page__wrapper">
+      <div
+      v-for="page in totalPage"
+      :key="page"
+      class="page"
+      >{{ page }}</div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +47,10 @@
         dialogVisible: false,
         isPostLoading: false,
         selectedSort:'',
+        searchQuiery:'',
+        page:1,
+        limit:10,
+        totalPage:0,
 
         sortOptions: [
           {value: 'title', name: 'По названию'},
@@ -71,9 +85,14 @@
         async fetchPosts(){
           try {
             this.isPostLoading = true;
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+              params:{
+                _page: this.page,
+                _limit: this.limit
+              }
+            });
+            this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit);
             this.posts = response.data;
-            this.isPostLoading = false;
           } 
           catch (error) {
             alert('Ошибка на стороне сервера');
@@ -93,13 +112,18 @@
       mounted(){
         this.fetchPosts();
       },
+      computed: {
+        sortedPosts(){
+          return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]));
+        },
 
-      watch: {
-        selectedSort(newValue){
-          this.posts.sort((post1, post2) => {
-            return post1[newValue]?.localeCompare(post2[newValue]);
-          });
+        searchedSortedPosts(){
+          return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuiery.toLowerCase()));
         }
+      },
+      
+      watch: {
+        
       }
 
   }
@@ -128,5 +152,18 @@
   .app__buttons{
     display:flex;
     justify-content: space-between;
+  }
+
+  .page__wrapper{
+    display: flex;
+  }
+
+  .page{
+    padding: 10px;
+    border: solid black 1px;
+  }
+
+  .current__page{
+    border: solid red 1px;
   }
 </style>
